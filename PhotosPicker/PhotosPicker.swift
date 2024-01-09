cfa09e8faa0a719ca17e9222db1f68ec15fc5751
Elaidzha1940 <eli080293@gmail.com>
2024-01-10 00:59:55 +0300

    Add PhotosPickerViewModel

--- PhotosPicker/PhotosPicker.swift
+++ PhotosPicker/PhotosPicker.swift
@@ -8,12 +8,32 @@
 //  */
 
 import SwiftUI
+import PhotosUI
+
+@MainActor
+final class PhotosPickerViewModel: ObservableObject {
+    @Published private(set) var selectedImage: UIImage? = nil
+    @Published var imageSelection: PhotosPickerItem? = nil
+}
 
 struct PhotosPicker: View {
+    @StateObject private var viewModel = PhotosPickerViewModel()
+    
     var body: some View {
-        
-        VStack {
+        VStack(spacing: 20) {
+            Text("Hooo")
             
+            if let image = viewModel.selectedImage {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+                    .cornerRadius(15)
+            }
+            PhotosPicker(selection: $viewModel.imageSelection) {
+                Text("Open the photo picker")
+                    .foregroundStyle(.black)
+            }
         }
     }
 }
