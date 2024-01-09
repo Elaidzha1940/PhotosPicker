3ffa002a73a20c31434b6b0f2535eafaa7306569
Elaidzha1940 <eli080293@gmail.com>
2024-01-10 00:35:17 +0300

    PhotosPicker

---
+++ PhotosPicker/PhotosPicker.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: PhotosPicker
+//  File: PhotosPicker.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.01.2024
+//
+//  */
+
+import SwiftUI
+
+struct PhotosPicker: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    PhotosPicker()
+}
---
+++ PhotosPicker/PhotosPickerApp.swift
@@ -0,0 +1,18 @@
+//  /*
+//
+//  Project: PhotosPicker
+//  File: PhotosPickerApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 10.01.2024
+//
+//  */
+import SwiftUI
+
+@main
+struct PhotosPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            PhotosPicker()
+        }
+    }
+}
